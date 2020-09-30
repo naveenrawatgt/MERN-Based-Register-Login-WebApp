dac4af62feef67e5a3d33fdaab6119950f999e11
naveenrawatgt <46143389+naveenrawatgt@users.noreply.github.com>
2020-09-30 22:10:06 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoDbURI : 'mongodb+srv://naveen:1947Rawat@cluster0-aesev.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    //mongoDbURI : <Connection string here>,
 }
